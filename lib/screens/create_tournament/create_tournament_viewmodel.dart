866d0bd97d0ac64c4fc735ce8ee4eb58cf1dbe86
Talal <TalalOthman2000@gmail.com>
2021-06-17 12:38:37 +0800

    added 'create_tournament_viewmodel' file

---
+++ lib/screens/create_tournament/create_tournament_viewmodel.dart
@@ -0,0 +1,30 @@
+import '../../models/tournament.dart';
+import '../viewmodel.dart';
+
+class CreateTournamentViewmodel extends Viewmodel {
+  Tournament _tournament = Tournament();
+
+  get tournament => _tournament;
+  set tournament(value) => _tournament = value;
+
+  get name => _tournament.name;
+  set name(value) {
+    _tournament.name = value;
+  }
+
+  get skillLevel => _tournament.skillLevel;
+  set skillLevel(value) {
+    _tournament.skillLevel = value;
+  }
+
+  get prizePool => _tournament.prizePool;
+  set prizePool(value) {
+    _tournament.prizePool = value;
+  }
+
+  get description => _tournament.description;
+  set description(value) {
+    _tournament.description = value;
+  }
+
+}
