f3d63854d9ad99c99cda30572fa70462f844a497
Talal <TalalOthman2000@gmail.com>
2021-06-17 12:57:21 +0800

    updated 'body.dart' related to 'create_tournament'

--- lib/screens/create_tournament/widgets/body.dart
+++ lib/screens/create_tournament/widgets/body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import '../create_tournament_screen.dart';
+import '../create_tournament_viewmodel.dart';
+import '../../../models/tournament.dart';
+import '../../view.dart';
+
+
 
 
 class Body extends StatelessWidget {
@@ -9,148 +13,151 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Form(
-            key: formKey,
-            // autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // text
+    return View(
+      viewmodel: CreateTournamentViewmodel(),
+      builder:  (context, viewmodel, _) => SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Form(
+              key: formKey,
+              // autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // text
 
-                SizedBox(height: 60),
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Tournament Name",
-                    border: OutlineInputBorder(),
-                    // contentPadding:
-                  ),
-                  onChanged: (value){
-
-                  },
-                  validator: (value){
-                    if(value.isEmpty){
-                      return "Required";
-                    } else{
-                      return null;
-                    }
-                  },
-                ),
-
-                SizedBox(height: margin),
-                
-                DropdownButtonFormField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                  items: [
-                    DropdownMenuItem(
-                      child: Text("Beginner"),
-                      value: "Beginner",
+                  SizedBox(height: 60),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Tournament Name",
+                      border: OutlineInputBorder(),
+                      // contentPadding:
                     ),
-                    DropdownMenuItem(
-                      child: Text("Intermediate"),
-                      value: "Intermediate",
-                    ),
-
-                    DropdownMenuItem(
-                      child: Text("Advanced"),
-                      value: "Advanced",
-                    ),
-                  ],
-                  onChanged: (value) {
-                    
-                  },
-                  hint: Text("Skill Level"),
-                  //put a value here!
-                  validator: (value) {
-                    if (value == null) {
-                      return "Please select your skill level !";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-
-                SizedBox(height: margin),
-
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "Prize Pool",
-                    border: OutlineInputBorder(),
-                    // contentPadding:
-                  ),
-                  onChanged: (value){
-
-                  },
-                  validator: (value){
-                    if(value.isEmpty){
-                      return "Required";
-                    } else{
-                      return null;
-                    }
-                  },
-                ),
-
-                SizedBox(height: margin),
-
-                TextFormField(
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  minLines: 5,
-                  decoration: InputDecoration(
-                    labelText: "Description",
-                    border: OutlineInputBorder(),
-                    // contentPadding:
-                  ),
-                  onChanged: (value){
-
-                  },
-                  validator: (value){
-                    if(value.isEmpty){
-                      return "Required";
-                    } else{
-                      return null;
-                    }
-                  },
-                ),
-
-                SizedBox(height: 50),
-
-                SizedBox(
-                  width: double.maxFinite,
-                  
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (formKey.currentState.validate()) {
-                        print("Proceed to submit !");
+                    onChanged: (value){
+                      viewmodel.name = value;
+                    },
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Required";
+                      } else{
+                        return null;
                       }
                     },
-                    child: Text(
-                      "Submit",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
+                  ),
+
+                  SizedBox(height: margin),
+                  
+                  DropdownButtonFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
                     ),
-                    style: ButtonStyle(
-                      padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(
-                          vertical: 15.0,
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("Beginner"),
+                        value: "Beginner",
+                      ),
+                      DropdownMenuItem(
+                        child: Text("Intermediate"),
+                        value: "Intermediate",
+                      ),
+
+                      DropdownMenuItem(
+                        child: Text("Advanced"),
+                        value: "Advanced",
+                      ),
+                    ],
+                    onChanged: (value) {
+                      viewmodel.skillLevel = value;
+                    },
+                    hint: Text("Skill Level"),
+                    //put a value here!
+                    validator: (value) {
+                      if (value == null) {
+                        return "Please select your skill level !";
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+
+                  SizedBox(height: margin),
+
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "Prize Pool",
+                      border: OutlineInputBorder(),
+                      // contentPadding:
+                    ),
+                    onChanged: (value){
+                      viewmodel.prizePool = value;
+                    },
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Required";
+                      } else{
+                        return null;
+                      }
+                    },
+                  ),
+
+                  SizedBox(height: margin),
+
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    minLines: 5,
+                    decoration: InputDecoration(
+                      labelText: "Description",
+                      border: OutlineInputBorder(),
+                      // contentPadding:
+                    ),
+                    onChanged: (value){
+                      viewmodel.description = value;
+                    },
+                    validator: (value){
+                      if(value.isEmpty){
+                        return "Required";
+                      } else{
+                        return null;
+                      }
+                    },
+                  ),
+
+                  SizedBox(height: 50),
+
+                  SizedBox(
+                    width: double.maxFinite,
+                    
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState.validate()) {
+                          print("Proceed to submit !");
+                        }
+                      },
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(
+                            vertical: 15.0,
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
 
 
 
 
-                ],
+                  ],
+              ),
             ),
           ),
-        ),
+      ),
     );
                 
   }
