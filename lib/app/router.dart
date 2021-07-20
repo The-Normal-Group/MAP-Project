a5613c0d6feab6912716250a2e21e0db230c9a57
unknown <Mohannad Nasser>
2021-07-20 13:59:33 +0800

    adding the interface for manage account

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -6,6 +6,7 @@ import '../screens/main/main_screen.dart';
 import '../screens/tournament_organizers_menu/tournament_organizer_screen.dart';
 import '../screens/team_organizers_menu/team_organizer_screen.dart';
 import '../screens/update_tournament/update_tournament_screen.dart';
+import '../screens/manage_account/manage_account_screen.dart';
 
 Route<dynamic> createRoute(settings) {
   switch (settings.name) {
@@ -15,6 +16,8 @@ Route<dynamic> createRoute(settings) {
 
     case '/login':
       return LoginScreen.route();
+    case '/manageAccount':
+      return ManageAccountScreen.route();
     case '/Register':
       return RegisterScreen.route();
     case '/AddTournament':
