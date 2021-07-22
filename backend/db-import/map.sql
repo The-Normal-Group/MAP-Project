affa220a1701978d5842d1a3998f83016fe5d56e
Omar <alkhair@graduate.utm.my>
2021-07-22 17:07:34 +0800

    updated sql import file

--- backend/db-import/map.sql
+++ backend/db-import/map.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2021 at 01:20 PM
+-- Generation Time: Jul 22, 2021 at 10:09 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
@@ -32,17 +32,24 @@ CREATE TABLE `teams` (
   `name` varchar(255) NOT NULL,
   `capacity` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `creator` int(11) NOT NULL
+  `creator` int(11) NOT NULL,
+  `tournament` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `teams`
 --
 
-INSERT INTO `teams` (`id`, `name`, `capacity`, `description`, `creator`) VALUES
-(2, 'team1 updated', 0, 'team 1 here', 0),
-(3, 'team2', 0, 'desc', 0),
-(5, 'team3', 2, 'team not by user 0', 1);
+INSERT INTO `teams` (`id`, `name`, `capacity`, `description`, `creator`, `tournament`) VALUES
+(2, 'team1 updated', 0, 'team 1 here', 0, 4),
+(3, 'team2', 0, 'desc', 0, 6),
+(5, 'team3', 2, 'team not by user 0', 1, 4),
+(6, 'teamOmar3', 0, 'team created by user omar3', 0, NULL),
+(7, 'team1', 3, 'random team', 33, NULL),
+(8, 'team3', 0, 'test test', 33, NULL),
+(9, 'team 4', 0, 'description here', 33, NULL),
+(10, 'team1', 3, 'random team', 33, NULL),
+(11, 'team1', 3, 'random team', 33, NULL);
 
 -- --------------------------------------------------------
 
@@ -65,7 +72,20 @@ CREATE TABLE `tournaments` (
 
 INSERT INTO `tournaments` (`id`, `name`, `skillLevel`, `prizePool`, `description`, `creator`) VALUES
 (4, 'update', 'Intermediate', 222, 'update', 0),
-(5, 'tournament2', 'Beginner', 444, 'Tournament not by user 0', 1);
+(5, 'tournament2', 'Beginner', 444, 'Tournament not by user 0', 1),
+(6, 'new tournament', 'Beginner', 244, 'A tournament hosted by the UTM Esports club.', 0),
+(7, 'tournament3', 'Intermediate', 222, 'tournament3', 0),
+(8, 'tournament5', 'Intermediate', 24242, 'teasfsa', 0),
+(9, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(10, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(11, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(12, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(13, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(14, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(15, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 0),
+(16, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 38),
+(17, 'tournament2', 'Intermediate', 234254, 'fsafasf', 38),
+(18, 'lol tournament', 'Advanced', 1234, 'bronze level tournament', 38);
 
 -- --------------------------------------------------------
 
@@ -78,17 +98,24 @@ CREATE TABLE `users` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `type` int(11) NOT NULL
+  `type` int(11) NOT NULL,
+  `team` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `type`) VALUES
-(29, 'nsfsafa', '123', 'fsa@gmail.com', 0),
-(33, 'omar', '123', 'omar@gmail.com', 0),
-(34, 'alex', '123', 'alex@gmail.com', 1);
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `type`, `team`) VALUES
+(29, 'nsfsafa', '123', 'fsa@gmail.com', 0, 9),
+(33, 'omar', '123', 'omar@gmail.com', 0, 2),
+(34, 'alex', '123', 'alex@gmail.com', 1, 7),
+(35, 'user3', '123', 'user3@gmail.com', 0, 2),
+(36, 'user4', '123', 'user3@gmail.com', 0, 2),
+(37, 'user5', '123', 'user3@gmail.com', 0, 8),
+(38, 'omar2', '123', 'omar@gmail.com', 1, NULL),
+(39, 'omar3', '123', 'omar@gmail.com', 0, NULL),
+(40, 'new30', '123', 'new30@gmail.com', 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -120,19 +147,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `tournaments`
 --
 ALTER TABLE `tournaments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
